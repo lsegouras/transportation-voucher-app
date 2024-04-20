e9f00445086d0d155397b95a477e134b38fc77ed
Laura Segouras <lausegouras@gmail.com>
2024-04-20 13:28:32 -0300

    fixing Text prop on Button

--- src/components/atoms/Button/index.jsx
+++ src/components/atoms/Button/index.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import * as C from "./styles";
 
 const Button = ({ Text, onClick, Type }) => {
-  return <C.Button type={Type} onClick={onClick} text={Text}></C.Button>;
+  return (
+    <C.Button type={Type} onClick={onClick}>
+      {Text}
+    </C.Button>
+  );
 };
 
 export default Button;
