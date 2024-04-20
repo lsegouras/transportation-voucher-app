b72db14f6b84b5ad3d669596a51a28187f7c7c87
Laura Segouras <lausegouras@gmail.com>
2024-04-20 13:31:15 -0300

    Creating login page and styles

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,7 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
+import * as C from "./styles";
+import Button from "../../components/atoms/Button";
+import Input from "./../../components/atoms/Input";
+import { Link, useNavigate } from "react-router-dom";
 
 const Login = () => {
-  return <div>Login</div>;
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleLogin = () => {
+    if (!email | !password) {
+      setError("Fill in all fields!");
+      return;
+    }
+
+    const res = console.log(email, password);
+
+    if (res) {
+      setError(res);
+      return;
+    }
+
+    navigate("/home");
+  };
+  return (
+    <C.Container>
+      <C.Title>Login System</C.Title>
+      <C.Content>
+        <Input
+          type="email"
+          placeholder="Enter your E-mail"
+          value={email}
+          onChange={(e) => [setEmail(e.target.value), setError("")]}
+        />
+        <Input
+          type="password"
+          placeholder="Enter your password"
+          value={password}
+          onChange={(e) => [setPassword(e.target.value), setError("")]}
+        />
+        <C.LabelError>{error}</C.LabelError>
+        <Button Text="Sign In" onClick={handleLogin} Type="button" />
+        <C.LabelSignup>
+          Don't have an account?
+          <C.Strong>
+            <Link to="/signup">&nbsp;Register</Link>
+          </C.Strong>
+        </C.LabelSignup>
+      </C.Content>
+    </C.Container>
+  );
 };
 
 export default Login;
