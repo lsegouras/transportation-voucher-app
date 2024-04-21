8f1c36607f0333ffec6cf031a1f779dab6db5f6d
Laura Segouras <lausegouras@gmail.com>
2024-04-21 11:43:15 -0300

    feat: adding Register page and styles

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,7 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
+import Input from "../../components/atoms/Input";
+import Button from "../../components/atoms/Button";
+import * as C from "./styles";
+import { Link, useNavigate } from "react-router-dom";
+import useAuth from "../../hooks/useAuth";
 
 const Register = () => {
-  return <div>Register</div>;
+  const navigate = useNavigate();
+  const { register } = useAuth();
+
+  const [email, setEmail] = useState("");
+  const [emailConf, setEmailConf] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSignup = () => {
+    if (!email | !emailConf | !password) {
+      setError("Fill in all fields!");
+      return;
+    } else if (email !== emailConf) {
+      setError("E-mails don't match!");
+      return;
+    }
+
+    const res = register(email, password);
+
+    if (res) {
+      setError(res);
+      return;
+    }
+
+    alert("User successfully registered!");
+    navigate("/");
+  };
+
+  return (
+    <C.Container>
+      <C.Title>Register System</C.Title>
+      <C.Content>
+        <Input
+          type="email"
+          placeholder="Enter your E-mail"
+          value={email}
+          onChange={(e) => [setEmail(e.target.value), setError("")]}
+        />
+        <Input
+          type="email"
+          placeholder="Confirm your E-mail"
+          value={emailConf}
+          onChange={(e) => [setEmailConf(e.target.value), setError("")]}
+        />
+        <Input
+          type="password"
+          placeholder="Enter your password"
+          value={password}
+          onChange={(e) => [setPassword(e.target.value), setError("")]}
+        />
+        <C.labelError>{error}</C.labelError>
+        <Button Text="Register" onClick={handleSignup} />
+        <C.LabelSignin>
+          Have already an account?
+          <C.Strong>
+            <Link to="/">&nbsp;Enter</Link>
+          </C.Strong>
+        </C.LabelSignin>
+      </C.Content>
+    </C.Container>
+  );
 };
 
 export default Register;
---
+++ src/pages/Register/styles.js
@@ -0,0 +1,60 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  gap: 10px;
+  height: 100vh;
+`;
+
+export const Content = styled.div`
+  gap: 15px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  width: 100%;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+  background-color: white;
+  max-width: 350px;
+  padding: 20px;
+  border-radius: 10px;
+
+  &:hover {
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+  }
+`;
+
+export const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 700;
+  color: #676767;
+`;
+
+export const LabelSignin = styled.label`
+  font-size: 16px;
+  color: #555;
+`;
+
+export const labelError = styled.label`
+  font-size: 14px;
+  color: #ff234f;
+  font-weight: 500;
+  margin-top: 8px;
+`;
+
+export const Strong = styled.strong`
+  cursor: pointer;
+  transition: 0.3s ease;
+
+  &:hover {
+    font-size: 17px;
+  }
+
+  a {
+    text-decoration: none;
+    color: #676767;
+  }
+`;
