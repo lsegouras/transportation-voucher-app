003c1ebbbb6364f704a49931aecaf3bfcea8e37e
Laura Segouras <lausegouras@gmail.com>
2024-04-21 12:14:07 -0300

    chore: importing and using Popping font from Google Fonts on global style

--- src/styles/global.js
+++ src/styles/global.js
@@ -2,6 +2,8 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
 
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap');
+
 * {
   margin: 0;
   padding: 0;
@@ -12,7 +14,7 @@ body{
   width: 100vw;
   height: 100vh;
   background-color: #f0f2f5;
-  font-family: Arial, Helvetica, sans-serif;
+  font-family: 'Poppins', sans-serif;
 }
 `;
 
