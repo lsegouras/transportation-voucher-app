2b9d551f4725af0aea6f63d394bfcbd55e815ea6
Laura Segouras <lausegouras@gmail.com>
2024-04-21 11:40:59 -0300

    chore: update Button and Input styles for improved appearance and accessibility

--- src/components/atoms/Button/styles.js
+++ src/components/atoms/Button/styles.js
@@ -7,9 +7,14 @@ export const Button = styled.button`
   border-radius: 5px;
   width: 100%;
   cursor: pointer;
-  background-color: #046ee5;
+  background-color: #ff5f6d;
   color: white;
   font-weight: 600;
   font-size: 16px;
   max-width: 350px;
+  transition: all 0.2s ease-in-out;
+
+  &:hover {
+    background-color: #ff434f;
+  }
 `;
--- src/components/atoms/Input/styles.js
+++ src/components/atoms/Input/styles.js
@@ -5,6 +5,10 @@ export const Input = styled.input`
   padding: 16px 20px;
   width: 100%;
   border-radius: 5px;
-  background-color: #f0f2f5;
-  border: none;
+  background-color: #f8f9fa;
+  border: 1px solid #f8f9fa;
+  transition: border-color 0.3s ease;
+  &:focus {
+    border-color: #ff5f6d;
+  }
 `;
