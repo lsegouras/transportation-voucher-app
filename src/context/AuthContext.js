f681c7f4853aa4e7271bfba76d0004da0add8e1a
Laura Segouras <lausegouras@gmail.com>
2024-04-21 11:27:44 -0300

    Creating AuthContext to manage login, register and signout

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,39 +1,92 @@
-import React, { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
-export const UserAuthContext = createContext();
+export const AuthContext = createContext({});
 
-const UserAuthContextProvider = ({ children }) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [user, setUser] = useState(null);
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState();
 
-  const login = (userData) => {
-    setIsLoading(true);
-    // logic to authenticate user
-    setTimeout(() => {
-      setIsLoggedIn(true);
-      setUser(userData);
-      setIsLoading(false);
-    }, 1000);
+  useEffect(() => {
+    try {
+      const userToken = localStorage.getItem("user_token");
+      const usersStorage = localStorage.getItem("users_bd");
+
+      if (userToken && usersStorage) {
+        const hasUser = JSON.parse(usersStorage)?.filter(
+          (user) => user.email === JSON.parse(userToken).email
+        );
+
+        if (hasUser?.length) setUser(hasUser[0]);
+      }
+    } catch (error) {
+      console.error("Error loading user session: ", error);
+    }
+  }, []);
+
+  const login = (email, password) => {
+    try {
+      const usersStorage = JSON.parse(localStorage.getItem("users_bd"));
+
+      const hasUser = usersStorage?.filter((user) => user.email === email);
+
+      if (hasUser?.length) {
+        if (hasUser[0].email === email && hasUser[0].password === password) {
+          const token = Math.random().toString(36).substring(2);
+          localStorage.setItem("user_token", JSON.stringify({ email, token }));
+          setUser({ email, password });
+          return;
+        } else {
+          return "Email or password incorrect!";
+        }
+      } else {
+        return "User not registered!";
+      }
+    } catch (error) {
+      console.error("Error during login: ", error);
+      return error.massage;
+    }
   };
 
-  const logout = () => {
-    setIsLoading(true);
-    // logic to logout user
-    setTimeout(() => {
-      setIsLoggedIn(false); 
+  const register = (email, password) => {
+    try {
+      const usersStorage = JSON.parse(localStorage.getItem("users_bd"));
+
+      const hasUser = usersStorage?.filter((user) => user.email === email);
+
+      if (hasUser?.length) {
+        return "User with this email already exists!";
+      }
+
+      let newUser;
+
+      if (usersStorage) {
+        newUser = [...usersStorage, { email, password }];
+      } else {
+        newUser = [{ email, password }];
+      }
+
+      localStorage.setItem("users_bd", JSON.stringify(newUser));
+
+      return;
+    } catch (error) {
+      console.error("Error during registration: ", error);
+      return error.message;
+    }
+  };
+
+  const signout = () => {
+    try {
       setUser(null);
-      setIsLoading(false);
-    }, 1000);
+      localStorage.removeItem("user_token");
+    } catch (error) {
+      console.error("Error signing out: ", error);
+    }
   };
 
   return (
-    <UserAuthContext.Provider
-      value={{ isLoggedIn, user, login, logout, isLoading }}
+    <AuthContext.Provider
+      value={{ user, signed: !!user, login, register, signout }}
     >
       {children}
-    </UserAuthContext.Provider>
+    </AuthContext.Provider>
   );
 };
-
-export default UserAuthContextProvider;
