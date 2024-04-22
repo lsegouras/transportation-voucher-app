a1f3bcfb6ee827754ebdc52de5476b6e11bfe092
Laura Segouras <lausegouras@gmail.com>
2024-04-22 09:29:34 -0300

    chore: updating Home page and styles

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import Button from "../../components/atoms/Button";
+import useAuth from "../../hooks/useAuth";
+import * as C from "./styles";
+import Navigation from "../../components/organisms/Navbar";
+import { AuthContext } from "../../context/AuthContext";
 
 const Home = () => {
-  return <div>Home</div>;
+  const { signout } = useAuth();
+  const navigate = useNavigate();
+  const { user } = useContext(AuthContext);
+
+  return (
+    <>
+      <Navigation />
+      <C.Container>
+        <C.Title>Home</C.Title>
+        {user && <C.WelcomeMessage>Welcome, {user.fullName}!</C.WelcomeMessage>}
+        <Button Text="Employees" onClick={() => navigate("/employees")} />{" "}
+        <Button Text="Leave" onClick={() => [signout(), navigate("/")]} />{" "}
+      </C.Container>
+    </>
+  );
 };
 
 export default Home;
---
+++ src/pages/Home/styles.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  height: 100vh;
+  gap: 20px;
+`;
+
+export const Title = styled.h2`
+  color: #333;
+  font-size: 32px;
+  margin-bottom: 20px;
+`;
+
+export const WelcomeMessage = styled.p`
+  color: #555;
+  font-size: 24px;
+  margin-bottom: 40px;
+`;
