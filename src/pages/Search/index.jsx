139360c8911d7b6dc7af5bd06fe086de64ddaf41
Laura Segouras <lausegouras@gmail.com>
2024-04-23 19:45:04 -0300

    feat: creating Search Page and styles

--- src/pages/Search/index.jsx
+++ src/pages/Search/index.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import SearchBar from "../../components/molecules/SearchBar/index";
+import EmployeeList from "../../components/organisms/EmployeesList/index";
+import Button from "../../components/atoms/Button";
+import { Link } from "react-router-dom";
+import * as C from "./styles";
 
 const Search = () => {
-  return <div>Search</div>;
+  return (
+    <C.Container>
+      <C.Title>Employees List</C.Title>
+      <SearchBar />
+      <EmployeeList />
+      <Link to="/employees">
+        <Button Text="Back to Employees" />
+      </Link>
+    </C.Container>
+  );
 };
 
 export default Search;
---
+++ src/pages/Search/styles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  gap: 20px;
+  height: 100%;
+`;
+
+export const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 700;
+  color: #676767;
+  margin-top: 30px;
+`;
