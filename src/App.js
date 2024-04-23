989391a7e04eaa85eeb5a141229371c0773471dc
Laura Segouras <lausegouras@gmail.com>
2024-04-23 09:21:36 -0300

    chore: update App.js - importing DataContext

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from "react";
 import GlobalStyle from "./styles/global";
 import RoutesApp from "./routes";
 import { AuthProvider } from "./context/AuthContext";
+import { DataProvider } from "./context/DataContext";
 
 const App = () => {
   return (
     <AuthProvider>
-      <RoutesApp />
+      <DataProvider>
+        <RoutesApp />
+      </DataProvider>
       <GlobalStyle />
     </AuthProvider>
   );
